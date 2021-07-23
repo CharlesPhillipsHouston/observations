1bdd496fe0ebe072e3eab72592502cacfd52e97d
CharlesPhillipsHouston <charles@spaceflightresearch.net>
2021-07-22 20:40:28 -0500

    Initial Commit

---
+++ observations/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  observations
+//
+//  Created by Charles Phillips on 7/22/21.
+//  Copyright © 2021 Charles Phillips. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
